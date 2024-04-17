e10e517b0e282067da87eee6a75267d2884914dc
sameerishaare <sameergupta4873@gmail.com>
2024-04-17 19:58:58 +0530

    build error resolved

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
     <main className="flex max-h-screen flex-col items-center justify-start p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Note: Devnet / Testnet SOL aren't real SOL.
+          Note: Devnet / Testnet SOL are not real SOL.
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -103,8 +103,9 @@ export default function Home() {
               </button>
               {openAmount && (
                 <div className="absolute p-3.5 border border-neutral-700/60 rounded-md bg-black mt-1 flex flex-wrap gap-2.5 w-[7.6rem]">
-                  {sols.map((sol) => (
+                  {sols.map((sol, idx) => (
                     <button
+                      key={idx}
                       onClick={() => {
                         setAmount(sol);
                         setOpenAmount(false);
